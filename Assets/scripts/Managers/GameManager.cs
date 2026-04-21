0b504f5ebeab2ddeb36664a65be1ffaf1c98a564
来某人 <2451611152@qq.com>
2026-04-21 16:18:42 +0800

    调整祝福“短视”

--- Assets/scripts/Managers/GameManager.cs
+++ Assets/scripts/Managers/GameManager.cs
@@ -84,7 +84,7 @@ public class GameManager : MonoBehaviour
         // 重置本局统计数据
         ResetRoundStatistics();
 
-        currentPoints = 0;
+        currentPoints = 100000000;
 
         currentRound = 1;
 
@@ -339,9 +339,6 @@ public class GameManager : MonoBehaviour
         if (blessingManager != null)
         {
             float blessingMultiplier = blessingManager.GetFinalBlessingMultiplier();
-            //祝福——短视：每回合倍率-1，可叠加
-            int count = blessingManager.GetBlessingCount(BlessingData.BlessingType.ShortSight);
-            blessingMultiplier -= count;
             //赌神传说
             blessingMultiplier += blessingManager.GetGodOfGamblerTempMultiplier();
             multiplier += blessingMultiplier;
@@ -467,6 +464,11 @@ public class GameManager : MonoBehaviour
         currentState = GameState.Shop;
         shopManager.OpenShop();
         ChangeState(GameState.Shop);
+        // 短视倍率缩减
+        if (blessingManager != null)
+        {
+            blessingManager.OnNewRound_ShortSightDecay();
+        }
     }
 
     /// <summary>
